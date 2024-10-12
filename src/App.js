e30bdcabcd873531a1294a811b3cdfbe50e4e058
smin987 <smin987@gmail.com>
2024-10-12 13:09:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id"
+          element={<Detail />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/`}
+          element={< Home />}>
+        </Route>
+      </Routes>
+    </Router >
+  )
 }
 
 export default App;
+
+// npm i gh-pages 를 인스톨 하여 git hub에 웹을 발행하자.
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
 function Detail() {
+    const { id } = useParams()
+    const url = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    const getMovie = async () => {
+        const json = await (
+            await fetch(url)
+        ).json()
+        console.log(json)
+    }
+    useEffect(() => {
+        getMovie()
+    }, [])
     return <h1>Detail</h1>
 }
 
